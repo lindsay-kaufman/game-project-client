20c3c0a5d4aa7c037647a88b4b17423494003494
lindsay-kaufman <linds234@gmail.com>
2020-01-06 10:28:27 -0500

    Add auth forms and curl scripts
    Added auth forms and curl scripts, committing early to test deployment.
    Did not finish feature.

---
+++ assets/scripts/events.js
@@ -0,0 +1,55 @@
+const getFormFields = require('../../lib/get-form-fields')
+const ui = require('./ui')
+const api = require('./api')
+
+const onSignUp = function () {
+  event.preventDefault()
+  const form = event.target
+  // get data from sign up form
+  const data = getFormFields(form)
+  // send form data to api to store new user
+  api.signUp(data)
+    .then(ui.signUpSuccessful)
+    .catch(ui.signUpFailure)
+}
+
+const onSignIn = function () {
+  event.preventDefault()
+  const form = event.target
+  // get data from sign in form
+  const data = getFormFields(form)
+  // send to api
+  api.signIn(data)
+    .then(ui.signInSuccessful)
+    .catch(ui.signInFailure)
+}
+
+const onChangePassword = function () {
+  event.preventDefault()
+  const form = event.target
+  // get new password data
+  const data = getFormFields(form)
+  // send new password data to api
+  api.changePassword(data)
+    .then(ui.changePasswordSuccessful)
+    .catch(ui.changePasswordFailure)
+}
+
+const onSignOut = function () {
+  event.preventDefault()
+
+  api.signOut()
+    .then(ui.signOutSuccessful)
+    .catch(ui.signOutFailure)
+}
+
+const addHandlers = function () {
+  $('#sign-up').on('submit', onSignUp)
+  $('#sign-in').on('submit', onSignIn)
+  $('#change-password').on('submit', onChangePassword)
+  $('#sign-out').on('submit', onSignOut)
+}
+
+module.exports = {
+  addHandlers
+}
---
+++ assets/scripts/ui.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const store = require('./store')
+
+const signUpSuccessful = function () {
+
+}
+const signUpFailure = function () {}
+const signInSuccessful = function () {}
+const signInFailure = function () {}
+const changePasswordSuccessful = function () {}
+const changePasswordFailure = function () {}
+const signOutSuccessful = function () {}
+const signOutFailure = function () {}
+
+module.exports = {
+  signUpSuccessful,
+  signUpFailure,
+  signInSuccessful,
+  signInFailure,
+  changePasswordSuccessful,
+  changePasswordFailure,
+  signOutSuccessful,
+  signOutFailure
+}
